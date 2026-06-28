fe8eb60a1e052f147b8b5f7fc9b5615b68679041
gooberdagoatsimonareureadingthis <mehrnagi@gmail.com>
2026-06-28 16:55:09 -0700

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -6,3 +6,9 @@
 //GPT caught a ton (5?) silly mistakes in cannon and bullet loop
 //GPT caught that i wasnt storing bullet sounds in vector (for person bullets)
 //Overall, this is a beginner project.
+//music/sound 
+  //main music is the instrumental version of monëy so big by yeat downloaded form this youtube video:
+  //https://www.youtube.com/watch?v=QEm-GiEeUP0
+  //bomb sound effect from https://www.youtube.com/watch?v=rGjIlKHcC4A&list=PLhDaYj5fPnIqLNz8gN-MA1J2tWFm99JbH&index=3
+  //enemy bullet sound effect from https://www.youtube.com/watch?v=k-ghS5Juqpo
+  //player bullet sound effect from https://www.youtube.com/watch?v=0ys3cDI9cBc
