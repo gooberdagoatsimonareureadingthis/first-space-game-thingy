5b01d1147810a89f33fd0cb1bd7ad6dbcd5e6657
gooberdagoatsimonareureadingthis <mehrnagi@gmail.com>
2026-06-28 16:53:51 -0700

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,3 +1,8 @@
-/*
-
-*/
+//GPT did I--; AND CONTINUE IN EVERY LOOP;
+//GPT FIXED RANDOMCANNON RANGE FROM (0, 8) TO (0, 7)
+//GPT CAUGHT THE SILLY ERROR OF ME DRAWING THE BASE AFTER TEXT AND MADE THE ENEMY TEXTS RENDER
+//GPT caught that i didnt erase bullets after collision with cannon
+//GPT provided explosion time subtraction to act as a delay.
+//GPT caught a ton (5?) silly mistakes in cannon and bullet loop
+//GPT caught that i wasnt storing bullet sounds in vector (for person bullets)
+//Overall, this is a beginner project.
